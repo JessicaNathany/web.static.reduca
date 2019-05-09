bd83621677a6532ebbfade7d3f009ccac0a96633
wsantos <wsantos@EMC083171.cijunsp.local>
2019-05-09 17:46:01 -0300

    Estrutura de pastas adicionar o template

--- app/view/Layout.php
+++ app/view/Layout.php
@@ -24,7 +24,7 @@
 
     </head>
     <body>
-    <!--=================[HEADER SUPERIOR]====================-->    
+        <!--=================[HEADER SUPERIOR]====================-->    
         <div class="site-wrap">
             <div class="site-mobile-menu">
                 <div class="site-mobile-menu-header">
@@ -56,74 +56,88 @@
                 </div> 
             </div>
         </div>
-            <!--=================[HEADER]====================-->
-            <div class="Header">
-                <?= $this->addHeader(); ?>
-            </div>
+        <!--=================[HEADER]====================-->
+        <div class="Header">
+            <?= $this->addHeader(); ?>
+        </div>
 
 
-            <!--=================[MAIN]====================-->
-            <div class="Main">
-                <?= $this->addMain(); ?>
-            </div> 
+        <!--=================[MAIN]====================-->
+        <div class="Main">
+            <?= $this->addMain(); ?>
+        </div> 
 
 
-            <!--=================[FOOTER]====================-->
-            <footer class="site-footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-9">
-                            <div class="row">
-                                <div class="col-md-5">
-                                    <h2 class="footer-heading mb-4">About Us</h2>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
-                                </div>
-                                <div class="col-md-3 ml-auto">
-                                    <h2 class="footer-heading mb-4">Features</h2>
-                                    <ul class="list-unstyled">
-                                        <li><a href="#">About Us</a></li>
-                                        <li><a href="#">Services</a></li>
-                                        <li><a href="#">Testimonials</a></li>
-                                        <li><a href="#">Contact Us</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-3">
-                                    <h2 class="footer-heading mb-4">Follow Us</h2>
-                                    <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
-                                    <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
-                                    <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                                    <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
-                                </div>
+        <!--=================[FOOTER]====================-->
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div class="row">
+                            <div class="col-md-5">
+                                <h2 class="footer-heading mb-4">About Us</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque facere laudantium magnam voluptatum autem. Amet aliquid nesciunt veritatis aliquam.</p>
                             </div>
-                        </div>
-                        <div class="col-md-3">
-                            <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
-                            <form action="#" method="post">
-                                <div class="input-group mb-3">
-                                    <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
-                                    <div class="input-group-append">
-                                        <button class="btn btn-primary text-white" type="button" id="button-addon2">Send</button>
-                                    </div>
-                                </div>
-                            </form>
+                            <div class="col-md-3 ml-auto">
+                                <h2 class="footer-heading mb-4">Features</h2>
+                                <ul class="list-unstyled">
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Services</a></li>
+                                    <li><a href="#">Testimonials</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-3">
+                                <h2 class="footer-heading mb-4">Follow Us</h2>
+                                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+                            </div>
                         </div>
                     </div>
-                    <div class="row pt-5 mt-5 text-center">
-                        <div class="col-md-12">
-                            <div class="border-top pt-5">
-                                <p>
-                                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-                                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                </p>
+                    <div class="col-md-3">
+                        <h2 class="footer-heading mb-4">Subscribe Newsletter</h2>
+                        <form action="#" method="post">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control border-secondary text-white bg-transparent" placeholder="Enter Email" aria-label="Enter Email" aria-describedby="button-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary text-white" type="button" id="button-addon2">Send</button>
+                                </div>
                             </div>
-                        </div>
-
+                        </form>
                     </div>
                 </div>
-            </footer>
-            <div class="footer">
-                <?= $this->addFooter(); ?>
+                <div class="row pt-5 mt-5 text-center">
+                    <div class="col-md-12">
+                        <div class="border-top pt-5">
+                            <p>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+        </footer>
+        <div class="footer">
+            <?= $this->addFooter(); ?>
+        </div>
+        <script src="<?=DIRJS.'jquery-3.3.1.min.js'?>"></script>
+        <script src="<?=DIRJS.'jquery-migrate-3.0.1.min.js'?>"></script>
+        <script src="<?=DIRJS.'jquery-ui.js'?>"></script>
+        <script src="<?=DIRJS.'popper.min.js'?>"></script>
+        <script src="<?=DIRJS.'bootstrap.min.js'?>"></script>
+        <script src="<?=DIRJS.'owl.carousel.min.js'?>"></script>
+        <script src="<?=DIRJS.'jquery.stellar.min.js'?>"></script>
+        <script src="<?=DIRJS.'jquery.countdown.min.js'?>"></script>
+        <script src="<?=DIRJS.'jquery.magnific-popup.min.js'?>"></script>
+        <script src="<?=DIRJS.'bootstrap-datepicker.min.js'?>"></script>
+        <script src="<?=DIRJS.'aos.js'?>"></script>
+
+        <script src="<?=DIRJS.'typed.js'?>"></script>
+        <script src="<?=DIRJS.'main.js'?>"></script>
     </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -1,27 +1,10 @@
 <?php
 ob_start();
 header("Content-Type: text/html; charset=utf-8");
-/* 
- * https://www.youtube.com/watch?v=KkcUla_YjnI&list=PLbnAsJ6zlidvJZlF1M_rmFru3tFTn_8iF&index=7 -> MVC
- * https://www.youtube.com/watch?v=nzwp5oC7mgY&list=PLbnAsJ6zlidtE5NrT_9-q9VAPnwKsGPP_&index=2 -> Login
- * https://developers.google.com/recaptcha/docs/v3 ->RECAPTCH GOOGLE API
- * https://www.youtube.com/watch?v=GTywpgqSMrY -> paginação
- * https://materializecss.com/themes.html
- * 
- * email sgvmroot00@gmail.com
- * senha @dmin159357123
- */
 
 require_once ("../config/config.php");
 require_once ("../src/vendor/autoload.php");
 
-
-
-
-/**
- * 
- * 
- */
 $Dispatch=new App\Dispatch();
 
 
